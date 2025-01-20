231220b4078e8aa2d82bd7ecf8b398fd5d04895b
riya7272 <riya.2022ca083@mnnit.ac.in>
2025-01-20 15:58:18 +0530

    updated the files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:4000";
-// export const BASE_URL = "http://16.170.219.98/api";
+// export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "/api";
